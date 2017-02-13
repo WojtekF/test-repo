66b00da76457d4c85a88b915d193ab6ade30cb22
WojtekF <wfabianczuk@gmail.com>
2017-02-13 20:25:05 +0100

    initial commit for issue with istanbul

---
+++ src/DispatchableButton.tsx
@@ -0,0 +1,18 @@
+﻿// A '.tsx' file enables JSX support in the TypeScript compiler, 
+// for more information see the following page on the TypeScript wiki:
+// https://github.com/Microsoft/TypeScript/wiki/JSX
+
+import RaisedButton from "material-ui/RaisedButton";
+import * as React from "react";
+import { connect } from "react-redux";
+
+class DispatchButton extends React.Component<{label: string, onClick: () => void }, void> {
+    public render() {
+        return (
+            <RaisedButton label={this.props.label} onClick={this.props.onClick} />);
+    }
+}
+
+export default function(mapDispatchToProps: (dispatch: any, ownProps: any) => any) {
+    return connect(null, mapDispatchToProps)(DispatchButton);
+}
---
+++ src/__tests__/DispatchableButton.test.tsx
@@ -0,0 +1,44 @@
+﻿// A '.tsx' file enables JSX support in the TypeScript compiler, 
+// for more information see the following page on the TypeScript wiki:
+// https://github.com/Microsoft/TypeScript/wiki/JSX
+
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import getMuiTheme from "material-ui/styles/getMuiTheme";
+import * as React from "react";
+import * as TestUtils from "react-addons-test-utils";
+import { Provider } from "react-redux";
+import configureMockStore from "redux-mock-store"
+import createButton from "..//DispatchableButton";
+const store = configureMockStore()({});
+
+const mapDispatchToProps = (dispatch: any, ownProps: any) => { return { onClick: () => dispatch({ type: 'ADD_TODO', id: 0, text: 'test' }), ...ownProps } }
+const DB = createButton(mapDispatchToProps);
+
+describe("<DispatchableButton/>", () => {
+    let instance, container;
+
+    beforeEach(() => {
+        spyOn(store, "dispatch");
+        instance = TestUtils.renderIntoDocument(
+            <Provider store={store}>
+                <MuiThemeProvider muiTheme={getMuiTheme()}>
+                    <DB label={"some label"} />
+                </MuiThemeProvider>
+            </Provider>
+        );
+    });
+
+    it("dispatches action when clicked", () => {
+        container = TestUtils.findRenderedComponentWithType(instance, DB)
+        container.selector.props.onClick();
+        expect(store.dispatch).toHaveBeenCalledWith(
+            { type: "ADD_TODO", id: 0, text: "test" }
+        );
+    });
+
+    it("has label passed correctly", () => {
+        container = TestUtils.findRenderedComponentWithType(instance, DB)
+        let label = container.selector.props.label;
+        expect(label).toEqual("some label");
+    });
+});
